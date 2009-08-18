ae9e2460e59be94c0a6b956ca9737b855bfda565
Maciej Litwiniuk <maciej@galdomedia.pl>
2009-08-18 11:07:00 +0200

    initial commit

---
+++ jquery.links2objects-0.1.js
@@ -0,0 +1,81 @@
+(function($){
+ $.fn.links2objects = function(options) {
+    
+  var defaults = {
+    sites: ['youtube', 'flickr'],
+    flickr_size: 'medium',
+    flickr_api_key: '',
+    youtube_video_width: 560,
+    youtube_video_height: 340
+  };
+  var global_obj = null;
+  var options = $.extend(defaults, options);
+  
+
+  var flickr = function(html){
+    var ret_dict = {}
+    var link_regexp = /http:\/\/www.flickr.com\/photos\/([a-z]+)\/(\d+)\/?/gi;
+    var m = html.match(link_regexp);
+    if(m){
+      for(var i = 0; i<m.length; i++){
+        var photo_id = m[i].match(/\d+/)[0];
+        var request_link = 'http://api.flickr.com/services/rest/?format=json&method=flickr.photos.getSizes&photo_id='+photo_id+'&api_key='+options.flickr_api_key+'&jsoncallback=?';
+      
+        $.getJSON(request_link,
+          function(data){
+            if(data){
+              var sizes = data;
+              if(sizes.sizes && sizes.sizes.size && data.stat && data.stat=="ok"){
+                sizes = sizes.sizes.size;
+                for(var j=0;j<sizes.length; j++){
+                  if(sizes[j]["label"].toLowerCase()==options.flickr_size){
+                    var old_link = sizes[j].url.match(link_regexp)[0];
+                    var new_html = '<a href="'+old_link+'"><img src="'+sizes[j].source+'" /></a>';
+                    global_obj.html(global_obj.html().replace(new RegExp(old_link), new_html));
+                  }
+                }
+              }
+            }
+          }
+        
+        );
+      }
+    }
+  }
+  
+  var picasa = function(html){
+    var link_regexp = '';
+  }
+  
+  var youtube = function(html){
+    var link_regexp = /http:\/\/www.youtube.com\/watch\/?\?v=([a-z0-9\-\_]+)(&amp;[a-z]+=.*)*/gi;
+    var m = html.match(link_regexp);
+    if(m){
+      for(var i = 0; i<m.length; i++){
+        var video_id = m[i].match(/v=([a-zA-Z0-9]+)/i);
+        if(video_id[1]){
+          video_id = video_id[1];
+          var new_html = "<object width=\""+options.youtube_video_width+"\" height=\""+options.youtube_video_height+"\"> \
+          <param name=\"movie\" value=\"http://www.youtube.com/v/"+video_id+"&fs=1&rel=0\"></param> \
+          <param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param> \
+          <embed src=\"http://www.youtube.com/v/"+video_id+"&fs=1&rel=0\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\""+options.youtube_video_width+"\" height=\""+options.youtube_video_height+"\"></embed> \
+          </object>";
+          global_obj.html(global_obj.html().replace(m[i], new_html));
+        }
+      }
+    }
+  }
+  
+  var sites_functions = {'flickr': flickr, 'picasa': this.picasa, 'youtube': youtube };
+  
+  return this.each(function() {
+   obj = $(this);
+   global_obj = obj;
+   var body = obj.html();
+   for(var site in options.sites){
+     sites_functions[options.sites[site]](body);
+   }
+   //return this;
+  });
+ };
+})(jQuery);
---
+++ readme.txt
@@ -0,0 +1,7 @@
+links2objects
+
+License: LGPL
+Author: Maciej Litwiniuk <maciej at galdomedia dot pl>
+Webpage: http://galdomedia.pl/
+
+
